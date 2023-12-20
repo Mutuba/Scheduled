55543719a5adf886b0133ff5a575df3d75077c50
Mutuba <danielmutubait@gmail.com>
2023-12-20 15:05:21 +0300

    Add more user table fields

---
+++ db/migrate/20231220120432_add_fullname_timezone_avatar_url_uid_office_hours_start_office_hours_end_to_users.rb
@@ -0,0 +1,10 @@
+class AddFullnameTimezoneAvatarUrlUidOfficeHoursStartOfficeHoursEndToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :office_hours_start, :time
+    add_column :users, :office_hours_end, :time
+    add_column :users, :fullname, :string
+    add_column :users, :timezone, :string
+    add_column :users, :avatar_url, :string
+    add_column :users, :uid, :string
+  end
+end
