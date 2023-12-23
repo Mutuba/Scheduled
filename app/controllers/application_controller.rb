8c0a8ef3d076b28d26d0b6c46c82c20e488ad010
Mutuba <danielmutubait@gmail.com>
2023-12-23 10:20:58 +0300

    Refactor application controller set_timezone method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,35 +9,55 @@ class ApplicationController < ActionController::Base
   private
 
   def set_time_zone
-    user_timezone = session[:user_timezone]
-
-    if user_timezone.blank?
-      result = Geocoder.search(request.ip).first
-
-      if result&.data&.key?('timezone')
-        user_timezone = result.data['timezone']
-        session[:user_timezone] = user_timezone
-      else
-        sign_out(current_user)
-        flash[:alert] = 'Unable to determine your timezone. Please log in again.'
-        redirect_to new_user_session_path
-        return
-      end
-    end
+    user_timezone = determine_user_timezone
 
     if user_timezone.present?
-      Time.zone = user_timezone || 'UTC'
-      current_user.update!(timezone: user_timezone)
+      update_user_timezone(user_timezone)
     else
-      sign_out(current_user)
-      flash[:alert] = 'Invalid timezone information. Please log in again.'
-      redirect_to new_user_session_path
+      handle_invalid_timezone
     end
   rescue Geocoder::Error => e
-    flash[:alert] = "Geocoder error: #{e.message}"
-    redirect_to new_user_session_path
+    handle_geocoder_error(e)
   rescue StandardError => e
-    flash[:alert] = "Unexpected error: #{e.message}"
+    handle_unexpected_error(e)
+  end
+
+  def determine_user_timezone
+    session[:user_timezone].presence || fetch_user_timezone_from_geocoder
+  end
+
+  def fetch_user_timezone_from_geocoder
+    result = Geocoder.search(request.ip).first
+
+    if result&.data&.key?('timezone')
+      user_timezone = result.data['timezone']
+      session[:user_timezone] = user_timezone
+      user_timezone
+    else
+      handle_invalid_timezone
+    end
+  end
+
+  def update_user_timezone(user_timezone)
+    Time.zone = user_timezone || 'UTC'
+    current_user.update!(timezone: user_timezone)
+  end
+
+  def handle_invalid_timezone
+    sign_out_and_redirect('Invalid timezone information. Please log in again.')
+  end
+
+  def handle_geocoder_error(error)
+    sign_out_and_redirect("Geocoder error: #{error.message}")
+  end
+
+  def handle_unexpected_error(error)
+    sign_out_and_redirect("Unexpected error: #{error.message}")
+  end
+
+  def sign_out_and_redirect(message)
+    sign_out(current_user)
+    flash[:alert] = message
     redirect_to new_user_session_path
   end
 end
