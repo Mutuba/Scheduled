99f7fbe40c8954594832f75c8e5940b309deb4e9
Mutuba <danielmutubait@gmail.com>
2023-12-26 19:16:16 +0300

    Add omniauth request tests

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,10 +6,6 @@ module Users
       new_user_session_path
     end
 
-    def after_sign_in_path_for(_resource_or_scope)
-      dashboard_path
-    end
-
     def destroy
       session[:user_timezone] = nil if session[:user_timezone].present?
       super
